36e66afb647bb6328eba319486ebea6d9d60b736
sxcoder <color.ui>
2018-11-01 14:00:35 +0800

    isss
    :qi

--- test.java
+++ test.java
@@ -4,7 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer i=120;
-		Integer j=120;
+		Integer j=1200;
 		System.out.println(i==j);
 	}
 
